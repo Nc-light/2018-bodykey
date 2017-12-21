e206e2209c37b07df072aa2c2f59def8a5ef7967
Nc <nc@gzand-c.com>
2017-12-21 23:49:51 +0800

    ~~

---
+++ 2018-bodykey/test.ashx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace bodykey
+{
+
+    public class test : System.Web.IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            
+            context.Response.Write(context.Request["u"]+"<br><br>");
+            context.Response.Write(context.Request.QueryString["u"] + "<br><br>");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
